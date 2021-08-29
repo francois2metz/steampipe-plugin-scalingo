4d6d371444a0d3d84d7e1767f4be8ade4b56af45
François de Metz <francois@2metz.fr>
2021-08-30 00:24:48 +0200

    Add table key.

---
+++ docs/tables/scalingo_key.md
@@ -0,0 +1,15 @@
+# Table: scalingo_key
+
+List the SSH keys associated to your account.
+
+## Examples
+
+### List SSH Keys
+
+```sql
+select
+  id,
+  content
+from
+  scalingo_key;
+```
---
+++ scalingo/table_scalingo_key.go
@@ -0,0 +1,39 @@
+package scalingo
+
+import (
+	"context"
+
+	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/plugin"
+)
+
+func tableScalingoKey() *plugin.Table {
+	return &plugin.Table{
+		Name:        "scalingo_app_event",
+		Description: "Display the list of the regions accessible to your account.",
+		List: &plugin.ListConfig{
+			Hydrate: listKey,
+		},
+		Columns: []*plugin.Column{
+			{Name: "id", Type: proto.ColumnType_STRING, Description: "unique ID of the key"},
+			{Name: "name", Type: proto.ColumnType_STRING, Description: "Given name to the key"},
+			{Name: "content", Type: proto.ColumnType_STRING, Description: "Raw content of the SSH public key"},
+		},
+	}
+}
+
+func listKey(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
+	client, err := connect(ctx, d)
+	if err != nil {
+		return nil, err
+	}
+	keys, err := client.KeysList()
+	if err != nil {
+		return nil, err
+	}
+	for _, key := range keys {
+		d.StreamListItem(ctx, key)
+	}
+
+	return nil, nil
+}
